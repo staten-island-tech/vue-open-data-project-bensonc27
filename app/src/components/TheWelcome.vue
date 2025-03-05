ede902cae80f320b459b6a2bccce3357a2149b3b
bensonc27 <bensonc27@nycstudents.net>
2025-03-05 12:32:38 -0500

    found api i think

--- app/src/components/TheWelcome.vue
+++ app/src/components/TheWelcome.vue
@@ -2,6 +2,14 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+const death = ref("");
+async function finddeath() {
+  let res = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+  let data = await res.json();
+  death.value = data.results;
+}
+</script>
 
 <style lang="" scoped></style>
