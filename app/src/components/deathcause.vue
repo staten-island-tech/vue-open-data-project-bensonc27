bf93633978065b0dc19d22fa6ae91a76ef5e0d87
bensonc27 <bensonc27@nycstudents.net>
2025-03-06 11:48:14 -0500

    adding link

---
+++ app/src/components/deathcause.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link :to=""> </router-link>
+</template>
+
+<script setup>
+const props = defineProps({});
+
+const pkmnPath = computer(() => {
+  return `/pokemon/${props.id}`;
+});
+</script>
+
+<style scoped></style>
---
+++ app/src/views/DeathData.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Helo</h2>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const death = ref("");
+async function getDeath() {
+  let res = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+  let data = await res.json();
+  death.value = data.results;
+}
+</script>
+
+<style scoped></style>
